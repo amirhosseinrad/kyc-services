230214b2522af30b0adf81638a28da5b5350536b
amir-raad <raad@mail.com>
2025-09-06 17:58:32 +0330

    init

---
+++ src/main/java/ir/ipaam/kycservices/KycServicesApplication.java
@@ -0,0 +1,13 @@
+package ir.ipaam.kycservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KycServicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KycServicesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ir/ipaam/kycservices/KycServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.ipaam.kycservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KycServicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
