894511c737b11d960503f64b8b49c8d8ce0068c3
amir-raad <raad@mail.com>
2025-09-14 19:30:31 +0330

    add open-api

---
+++ src/main/java/ir/ipaam/kycservices/config/OpenApiConfig.java
@@ -0,0 +1,19 @@
+package ir.ipaam.kycservices.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI kycOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("KYC Services API")
+                        .version("v1.0.0")
+                        .description("API for KYC status verification and related operations"));
+    }
+}
