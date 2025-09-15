8a05180b9d51c738746bbeb0a59cc3d3dac2f72c
amir-raad <40541150+amirhosseinrad@users.noreply.github.com>
2025-09-15 19:38:28 +0330

    Handle KYC status update events

--- src/main/java/ir/ipaam/kycservices/infrastructure/handler/KycProcessEventHandler.java
+++ src/main/java/ir/ipaam/kycservices/infrastructure/handler/KycProcessEventHandler.java
@@ -1,17 +1,21 @@
 package ir.ipaam.kycservices.infrastructure.handler;
 
 import ir.ipaam.kycservices.domain.event.KycProcessStartedEvent;
+import ir.ipaam.kycservices.domain.event.KycStatusUpdatedEvent;
 import ir.ipaam.kycservices.domain.model.entity.Customer;
 import ir.ipaam.kycservices.domain.model.entity.KycProcessInstance;
+import ir.ipaam.kycservices.domain.model.entity.KycStepStatus;
 import ir.ipaam.kycservices.domain.query.FindKycStatusQuery;
 import ir.ipaam.kycservices.infrastructure.repository.CustomerRepository;
 import ir.ipaam.kycservices.infrastructure.repository.KycProcessInstanceRepository;
+import ir.ipaam.kycservices.infrastructure.repository.KycStepStatusRepository;
 import lombok.RequiredArgsConstructor;
 import org.axonframework.eventhandling.EventHandler;
 import org.axonframework.queryhandling.QueryHandler;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
+import java.util.Locale;
 
 @Component
 @RequiredArgsConstructor
@@ -19,6 +23,7 @@ public class KycProcessEventHandler {
 
     private final KycProcessInstanceRepository kycProcessInstanceRepository;
     private final CustomerRepository customerRepository;
+    private final KycStepStatusRepository kycStepStatusRepository;
 
     @EventHandler
     public void on(KycProcessStartedEvent event) {
@@ -36,6 +41,27 @@ public class KycProcessEventHandler {
         instance.setCustomer(customer);
 
         kycProcessInstanceRepository.save(instance);
+    }
+
+    @EventHandler
+    public void on(KycStatusUpdatedEvent event) {
+        kycProcessInstanceRepository.findByCamundaInstanceId(event.getProcessInstanceId())
+                .ifPresent(instance -> {
+                    instance.setStatus(event.getStatus());
+
+                    KycStepStatus stepStatus = new KycStepStatus();
+                    stepStatus.setProcess(instance);
+                    stepStatus.setStepName(event.getStepName());
+                    stepStatus.setTimestamp(event.getUpdatedAt());
+                    if (event.getState() != null) {
+                        stepStatus.setState(KycStepStatus.State.valueOf(event.getState().toUpperCase(Locale.ROOT)));
+                    }
+
+                    instance.getStatuses().add(stepStatus);
+
+                    kycProcessInstanceRepository.save(instance);
+                    kycStepStatusRepository.save(stepStatus);
+                });
     }
 
     @QueryHandler
