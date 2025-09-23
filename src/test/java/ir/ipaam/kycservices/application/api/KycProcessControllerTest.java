6eb3b04d847317b8b64e6fd4705fc7c96dd0015b
amir-raad <40541150+amirhosseinrad@users.noreply.github.com>
2025-09-23 14:53:34 +0330

    Handle resuming existing KYC process instances

--- src/test/java/ir/ipaam/kycservices/application/api/KycProcessControllerTest.java
+++ src/test/java/ir/ipaam/kycservices/application/api/KycProcessControllerTest.java
@@ -1,7 +1,11 @@
 package ir.ipaam.kycservices.application.api;
 
+import io.camunda.zeebe.client.ZeebeClient;
+import io.camunda.zeebe.client.api.command.CreateInstanceCommandStep1;
+import io.camunda.zeebe.client.api.response.ProcessInstanceEvent;
 import ir.ipaam.kycservices.application.api.controller.KycProcessController;
 import ir.ipaam.kycservices.application.api.error.ErrorCode;
+import ir.ipaam.kycservices.domain.command.StartKycProcessCommand;
 import ir.ipaam.kycservices.domain.model.entity.Customer;
 import ir.ipaam.kycservices.domain.model.entity.ProcessInstance;
 import ir.ipaam.kycservices.domain.model.entity.StepStatus;
@@ -9,7 +13,10 @@ import ir.ipaam.kycservices.infrastructure.service.KycServiceTasks;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import org.axonframework.commandhandling.gateway.CommandGateway;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -17,6 +24,9 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static ir.ipaam.kycservices.common.ErrorMessageKeys.KYC_STATUS_QUERY_FAILED;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyMap;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -29,6 +39,68 @@ class KycProcessControllerTest {
 
     @MockBean
     private KycServiceTasks tasks;
+
+    @MockBean
+    private CommandGateway commandGateway;
+
+    @MockBean
+    private ZeebeClient zeebeClient;
+
+    @Test
+    void startProcessResumesExistingInstance() throws Exception {
+        ProcessInstance instance = new ProcessInstance();
+        instance.setCamundaInstanceId("existing-id");
+        instance.setStatus("SELFIE_UPLOADED");
+
+        when(tasks.checkKycStatus("0024683416")).thenReturn(instance);
+
+        mockMvc.perform(post("/kyc/process")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"nationalCode\":\"0024683416\"}"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.processInstanceId").value("existing-id"))
+                .andExpect(jsonPath("$.status").value("SELFIE_UPLOADED"));
+
+        verify(tasks).checkKycStatus("0024683416");
+        verifyNoInteractions(zeebeClient);
+        verifyNoInteractions(commandGateway);
+    }
+
+    @Test
+    void startProcessCreatesNewInstanceWhenCompletedOrMissing() throws Exception {
+        ProcessInstance completedInstance = new ProcessInstance();
+        completedInstance.setCamundaInstanceId("completed-id");
+        completedInstance.setStatus("COMPLETED");
+
+        when(tasks.checkKycStatus("0024683416")).thenReturn(completedInstance);
+
+        CreateInstanceCommandStep1 step1 = mock(CreateInstanceCommandStep1.class);
+        CreateInstanceCommandStep1.CreateInstanceCommandStep2 step2 = mock(CreateInstanceCommandStep1.CreateInstanceCommandStep2.class);
+        CreateInstanceCommandStep1.CreateInstanceCommandStep3 step3 = mock(CreateInstanceCommandStep1.CreateInstanceCommandStep3.class);
+        ProcessInstanceEvent event = mock(ProcessInstanceEvent.class);
+
+        when(zeebeClient.newCreateInstanceCommand()).thenReturn(step1);
+        when(step1.bpmnProcessId("kyc-process")).thenReturn(step2);
+        when(step2.latestVersion()).thenReturn(step3);
+        when(step3.variables(anyMap())).thenReturn(step3);
+        when(step3.send()).thenReturn(CompletableFuture.completedFuture(event));
+        when(event.getProcessInstanceKey()).thenReturn(9876L);
+        when(commandGateway.send(any(StartKycProcessCommand.class))).thenReturn(CompletableFuture.completedFuture(null));
+
+        mockMvc.perform(post("/kyc/process")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"nationalCode\":\"0024683416\"}"))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.processInstanceId").value("9876"))
+                .andExpect(jsonPath("$.status").value("STARTED"));
+
+        ArgumentCaptor<StartKycProcessCommand> commandCaptor = ArgumentCaptor.forClass(StartKycProcessCommand.class);
+        verify(commandGateway).send(commandCaptor.capture());
+        StartKycProcessCommand command = commandCaptor.getValue();
+        assertEquals("9876", command.processInstanceId());
+        assertEquals("0024683416", command.nationalCode());
+        verify(tasks).checkKycStatus("0024683416");
+    }
 
     @Test
     void statusEndpointReturnsStatus() throws Exception {
